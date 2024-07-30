1a89fe821bb91b8cbf0c4715dc40af8f35bda93b
Hgenryp <hgenryp@gmail.com>
2024-07-30 12:03:56 -0500

    Prueba 3

--- app/src/main/java/com/example/reforaccion/VerifyEmail.java
+++ app/src/main/java/com/example/reforaccion/VerifyEmail.java
@@ -23,7 +23,7 @@ public class VerifyEmail extends AppCompatActivity {
         Button buttonCode = findViewById(R.id.enterCode);
 
         hotmailBtn = findViewById(R.id.hotmailBtn);
-        url = "https://support.microsoft.com/en-us/office/how-to-sign-in-to-hotmail-c378938b-c0eb-40c1-b99e-338dd9e776a8";
+        url = "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=156&ct=1722315715&rver=7.0.6738.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fnlp%3d1%26cobrandid%3dab0455a0-8d03-46b9-b18b-df2f57b9e44c%26deeplink%3dowa%252f%26RpsCsrfState%3d58e32e05-7262-c4a5-def3-9f5b4275ee5a&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=ab0455a0-8d03-46b9-b18b-df2f57b9e44c";
         hotmailBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,7 +34,7 @@ public class VerifyEmail extends AppCompatActivity {
         });
 
         emailBtn = findViewById(R.id.emailBtn);
-        url = "https://mail.google.com/mail/u/0/#inbox";
+        url = "https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Faccounts.google.com%2F&followup=https%3A%2F%2Faccounts.google.com%2F&ifkv=AdF4I74t2dvgxT1GLV-MYaUuuCLGIdlKpWJlQGwXz6bwECkBAhSCURn4vYF6MSo3sMhydhWEsSbP&passive=1209600&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S472975819%3A1722315397048642&ddm=0";
         emailBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
