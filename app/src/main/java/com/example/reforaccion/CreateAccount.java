33b593d65d288c921c4da1a1ef654555db6ec5e4
Hgenryp <hgenryp@gmail.com>
2024-08-07 23:29:05 -0500

    Empezando Modelo 1

--- app/src/main/java/com/example/reforaccion/CreateAccount.java
+++ app/src/main/java/com/example/reforaccion/CreateAccount.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class CreateAccount extends AppCompatActivity {
@@ -17,6 +20,10 @@ public class CreateAccount extends AppCompatActivity {
 
         Button buttonCreate = findViewById(R.id.buttonCreate);
         TextView termsC= findViewById(R.id.termsC);
+        EditText passwordEditText1 = findViewById(R.id.editTextTextPassword5);
+        EditText passwordEditText2 = findViewById(R.id.editTextTextPassword4);
+        ImageButton togglePasswordVisibilityButton1 = findViewById(R.id.imageButton3);
+        ImageButton togglePasswordVisibilityButton2 = findViewById(R.id.imageButton2);
 
         termsC.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +38,36 @@ public class CreateAccount extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Politicas.class);
                 startActivity(intent);
+            }
+        });
+
+        togglePasswordVisibilityButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (passwordEditText1.getInputType() == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                    passwordEditText1.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    togglePasswordVisibilityButton1.setImageResource(R.drawable.baseline_visibility_off_24);
+                } else {
+                    passwordEditText1.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    togglePasswordVisibilityButton1.setImageResource(R.drawable.baseline_visibility_24);
+                }
+                // Mueve el cursor al final del texto
+                passwordEditText1.setSelection(passwordEditText1.getText().length());
+            }
+        });
+
+        togglePasswordVisibilityButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (passwordEditText2.getInputType() == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                    passwordEditText2.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    togglePasswordVisibilityButton2.setImageResource(R.drawable.baseline_visibility_off_24);
+                } else {
+                    passwordEditText2.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    togglePasswordVisibilityButton2.setImageResource(R.drawable.baseline_visibility_24);
+                }
+                // Mueve el cursor al final del texto
+                passwordEditText2.setSelection(passwordEditText2.getText().length());
             }
         });
     }
--- app/src/main/java/com/example/reforaccion/MainActivity.java
+++ app/src/main/java/com/example/reforaccion/MainActivity.java
@@ -40,8 +40,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
         buttonLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
